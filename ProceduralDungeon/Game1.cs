b5884c8ff167084f38a603b4d8c729a2fede2000
sui <sui95986@gmail.com>
2026-01-09 16:29:38 -0600

    Basic lighting is working & have some models that have been exported correctly.  Need to export the others.  Normal maps dont work... thing we need a different shader besides the one that comes with BasicEffect in order for that information to work properly.

--- ProceduralDungeon/Game1.cs
+++ ProceduralDungeon/Game1.cs
@@ -31,11 +31,15 @@ public class Game1 : Game
     Matrix pillarTwoWorldMatrix;
     Matrix floorMatrix;
     Matrix wallTorchMatrix;
+    Matrix coloredBoxMatrix;
+    Matrix rockyBoxMatrix;
 
     Model pillarOneOriginMiddle;
     Model pillarTwoOriginBottom;
     Model floor;
     Model walltorch;
+    Model coloredBox;
+    Model rockyBox;
 
     public Game1()
     {
@@ -67,6 +71,8 @@ public class Game1 : Game
         pillarTwoOriginBottom = Content.Load<Model>("models/pillar_origin_bottom_blue");
         floor = Content.Load<Model>("models/floor");
         walltorch = Content.Load<Model>("models/wall_torch");
+        coloredBox = Content.Load<Model>("models/colored_box");
+        rockyBox = Content.Load<Model>("models/rocky_box");
     }
 
     protected void BuildMap()
@@ -77,6 +83,8 @@ public class Game1 : Game
         pillarTwoWorldMatrix = Matrix.CreateWorld(pillarTwoPosition, Vector3.Forward, Vector3.Up);
         floorMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Forward, Vector3.Up);
         wallTorchMatrix = Matrix.CreateWorld(new Vector3(0, 0, 1f), Vector3.Forward, Vector3.Up);
+        coloredBoxMatrix = Matrix.CreateWorld(new Vector3(1.25f, 0, 1f), Vector3.Forward, Vector3.Up);
+        rockyBoxMatrix = Matrix.CreateWorld(new Vector3(2.25f, 0, 1f), Vector3.Forward, Vector3.Up);
     }
 
     protected override void Update(GameTime gameTime)
@@ -146,12 +154,52 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Black);
-
-        pillarOneOriginMiddle.Draw(pillarOneWorldMatrix, viewMatrix, projectionMatrix);
-        // pillarTwoOriginBottom.Draw(pillarTwoWorldMatrix, viewMatrix, projectionMatrix);
-        floor.Draw(floorMatrix, viewMatrix, projectionMatrix);
-        walltorch.Draw(wallTorchMatrix, viewMatrix, projectionMatrix);
+        GraphicsDevice.Clear(Color.DarkCyan);
+        GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+        DrawModel(pillarOneOriginMiddle, pillarOneWorldMatrix);
+        DrawModel(floor, floorMatrix);
+        DrawModel(walltorch, wallTorchMatrix);
+        DrawModel(coloredBox, coloredBoxMatrix);
+        DrawModel(rockyBox, rockyBoxMatrix);
         base.Draw(gameTime);
+    }
+
+    void DrawModel(Model model, Matrix world)
+    {
+        foreach (var mesh in model.Meshes)
+        {
+            Matrix meshWorld = mesh.ParentBone.Transform * world;
+            foreach (BasicEffect effect in mesh.Effects)
+            {
+                effect.World = meshWorld;
+                effect.View = viewMatrix;
+                effect.Projection = projectionMatrix;
+
+                effect.EnableDefaultLighting();
+                effect.PreferPerPixelLighting = true;
+                effect.Alpha = 1.0f;
+
+
+                // Key light
+                effect.DirectionalLight0.Direction = new Vector3(-0.5265408f, -0.5735765f, -0.6275069f);
+                effect.DirectionalLight0.DiffuseColor = new Vector3(1, 0.9607844f, 0.8078432f);
+                effect.DirectionalLight0.SpecularColor = new Vector3(1, 0.9607844f, 0.8078432f);
+                effect.DirectionalLight0.Enabled = true;
+
+                // Fill light.
+                effect.DirectionalLight1.Direction = new Vector3(0.7198464f, 0.3420201f, 0.6040227f);
+                effect.DirectionalLight1.DiffuseColor = new Vector3(0.9647059f, 0.7607844f, 0.4078432f);
+                effect.DirectionalLight1.SpecularColor = Vector3.Zero;
+                effect.DirectionalLight1.Enabled = true;
+
+                // Back light.
+                effect.DirectionalLight2.Direction = new Vector3(0.4545195f, -0.7660444f, 0.4545195f);
+                effect.DirectionalLight2.DiffuseColor = new Vector3(0.3231373f, 0.3607844f, 0.3937255f);
+                effect.DirectionalLight2.SpecularColor = new Vector3(0.3231373f, 0.3607844f, 0.3937255f);
+                effect.DirectionalLight2.Enabled = true;
+            }
+
+            mesh.Draw();
+        }
     }
 }
